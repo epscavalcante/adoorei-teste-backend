c9eb5e808d2f0f1e0772974094820eb8d44d19c0
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2024-03-03 17:28:56 -0400

    chore: register Interface repository of product to use ProductEloquentRepository container service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\Eloquent\ProductEloquentRepository;
+use Core\Domain\Repositories\IProductRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(
+            IProductRepository::class,
+            ProductEloquentRepository::class
+        );
     }
 
     /**
