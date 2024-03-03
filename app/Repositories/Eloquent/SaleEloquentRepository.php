72c8cb5d6b47c8698debc4f2e8bcc98b118c8111
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2024-03-03 18:21:18 -0400

    add: SaleEloquentRepository

---
+++ app/Repositories/Eloquent/SaleEloquentRepository.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\Sale as SaleModel;
+use App\Models\SaleProduct as SaleProductModel;
+use Core\Domain\Entities\Sale;
+use Core\Domain\Entities\SaleProduct;
+use Core\Domain\Exceptions\SaleNotFoundException;
+use Core\Domain\Repositories\ISaleRepository;
+use Core\Domain\ValueObjects\Uuid;
+use Illuminate\Support\Facades\DB;
+
+class SaleEloquentRepository implements ISaleRepository
+{
+    protected $model;
+
+    public function __construct(SaleModel $model)
+    {
+        $this->model = $model;
+    }
+
+    public function insert(Sale $sale): void
+    {
+        $saleModelCreated = null;
+
+        DB::beginTransaction();
+
+        try {
+            $saleModelCreated = $this->model->create([
+                'id' => $sale->getId()->getValue(),
+                'status' => $sale->getStatus()->value,
+                'total' => $sale->getTotal(),
+            ]);
+
+            $this->syncProducts($saleModelCreated, $sale->getProducts());
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
+        }
+    }
+
+    public function list(): array
+    {
+        $salesModel = $this->model->with('productIds')->get();
+
+        return array_map(
+            callback: fn ($model) => $this->mapModelToEntity($model),
+            array: $salesModel->all()
+        );
+    }
+
+    public function find(Uuid $saleId): Sale
+    {
+        $saleFound = $this->_find($saleId);
+
+        if (!$saleFound) throw new SaleNotFoundException($saleId);
+
+        return $this->mapModelToEntity($saleFound);
+    }
+
+    public function update(Sale $sale): void
+    {
+        $saleModel = $this->_find($sale->getId());
+
+        if (!$saleModel) throw new SaleNotFoundException($sale->getId());
+
+        DB::beginTransaction();
+
+        try {
+            $saleModel->update([
+                'total' => $sale->getTotal(),
+                'status' => $sale->getStatus()->value,
+            ]);
+
+            $this->syncProducts($saleModel, $sale->getProducts());
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
+        }
+    }
+
+    private function mapModelToEntity(SaleModel $model): Sale
+    {
+        $products = array_map(
+            callback: fn (SaleProductModel $model) => SaleProduct::create(
+                productId: $model->product_id,
+                price: $model->price,
+                amount: $model->amount,
+                // total: $model->total,
+            ),
+            array: $model->productIds->all()
+        );
+
+        return Sale::restore(
+            saleId: $model->id,
+            total: $model->total,
+            products: $products,
+            status: $model->status
+        );
+    }
+
+    private function _find(Uuid $saleId)
+    {
+        return $this->model->with('productIds')->find($saleId->getValue());
+    }
+
+    /**
+     * @param array<SaleProduct> $products
+     */
+    private function syncProducts(SaleModel $model, array $products)
+    {
+        $model->productIds()->delete();
+
+        $productsToSync = array_map(
+            callback: fn (SaleProduct $product) => [
+                'product_id' => $product->productId->getValue(),
+                'sale_id' => $model->id,
+                'price' => $product->getPrice(),
+                'amount' => $product->getAmount(),
+                'total' => $product->getTotal()->getValue(),
+            ],
+            array: $products
+        );
+
+        $model->productIds()->createMany($productsToSync);
+    }
+};
---
+++ tests/Integration/App/Repositories/SaleEloquentRepositoryIntegrationTest.php
@@ -0,0 +1,112 @@
+<?php
+
+use App\Models\Product as ProductModel;
+use App\Models\Sale as SaleModel;
+use App\Models\SaleProduct as SaleProductModel;
+use App\Repositories\Eloquent\ProductEloquentRepository;
+use App\Repositories\Eloquent\SaleEloquentRepository;
+use Core\Domain\Entities\Product;
+use Core\Domain\Entities\SaleProduct;
+use Core\Domain\Entities\Sale;
+use Core\Domain\Exceptions\SaleNotFoundException;
+use Core\Domain\ValueObjects\Uuid;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+uses(DatabaseMigrations::class);
+
+describe('SaleEloquentIntegrationTest', function () {
+
+    test('Insert method', function () {
+        $saleRepository = new SaleEloquentRepository(
+            new SaleModel()
+        );
+        $productRepository = new ProductEloquentRepository(
+            new ProductModel()
+        );
+
+        $product = Product::create(
+            name: 'Product test',
+            description: 'Product test',
+            price: 150
+        );
+        $sale = Sale::create();
+        $sale->addProduct(SaleProduct::create(
+            productId: $product->getId()->getValue(),
+            // name: $product->name,
+            price: $product->getPrice(),
+            amount: 10
+        ));
+        $productRepository->insert($product);
+        $saleRepository->insert($sale);
+
+        expect($sale)->toBeInstanceOf(Sale::class);
+        expect(SaleModel::count())->toBe(1);
+        expect(SaleProductModel::count())->toBe(1);
+        $saleModel = SaleModel::first()->toArray();
+        expect($saleModel['id'])->toBe($sale->getId()->getValue());
+        expect($saleModel['total'])->toBe($sale->getTotal());
+    });
+
+    test('List method', function () {
+        $saleRepository = new SaleEloquentRepository(
+            new SaleModel()
+        );
+
+        $product1 = Product::create(
+            name: 'Product test',
+            description: 'desc',
+            price: 150
+        );
+
+        $product2 = Product::create(
+            name: 'Product test',
+            description: 'desc',
+            price: 200
+        );
+
+        $sale = Sale::create();
+        $sale->addProduct(
+            SaleProduct::create(
+                productId: $product1->getId(),
+                // name: $product1->name,
+                price: $product1->getPrice(),
+                amount: 10
+            )
+        );
+        $sale->addProduct(
+            SaleProduct::create(
+                productId: $product2->getId(),
+                // name: $product2->name,
+                price: $product2->getPrice(),
+                amount: 7
+            )
+        );
+
+        $saleRepository->insert($sale);
+
+        expect(SaleModel::count())->toBe(1);
+        expect(SaleProductModel::count())->toBe(2);
+        expect(SaleModel::first()->toArray()['id'])->toBe($sale->getId()->getValue());
+    });
+
+    describe('Find method', function () {
+        test('Should throw SaleNotFoundException', function () {
+            $saleRepository = new SaleEloquentRepository(new SaleModel());
+            $saleId = Uuid::create();
+
+            $saleRepository->find($saleId);
+        })->throws(SaleNotFoundException::class);
+
+        test('Should finds a sale', function () {
+            $sale = Sale::create();
+            $saleRepository = new SaleEloquentRepository(new SaleModel());
+            $saleRepository->insert($sale);
+
+            $saleFound = $saleRepository->find($sale->getId());
+            expect($saleFound)->toBeInstanceOf(Sale::class);
+            expect(
+                $sale->getId()->equals($saleFound->getId())
+            )->toBe(true);
+        });
+    });
+});
