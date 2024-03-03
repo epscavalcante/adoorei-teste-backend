cbd64d63bb7af4362775c3a09d875bd9a7a4c558
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2024-03-03 19:19:47 -0400

    docs: map resources schemas

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -5,6 +5,16 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @OA\Schema(
+ *     title="Product resource",
+ *     description="Product resource",
+ *     @OA\Property(property="id", type="string", example="0006faf6-7a61-426c-9034-579f2cfcfa83"),
+ *     @OA\Property(property="name", type="string", example="Produto de teste"),
+ *     @OA\Property(property="description", type="string", example="Descrição do produto de teste"),
+ *     @OA\Property(property="price", type="int", example="1200"),
+ * )
+ */
 class ProductResource extends JsonResource
 {
     /**
