5a66161e70ef8ccfbb8c501b9476707846f40837
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2024-03-03 20:03:44 -0400

    fixes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             ProductSeeder::class,
+            SaleSeeder::class,
+            SaleProductSeeder::class,
         ]);
     }
 }
